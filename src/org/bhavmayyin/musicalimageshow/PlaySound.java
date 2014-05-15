7e6651a1ce0111cef5902faf0f071851bb66cd98
bhavanacse <bhavana@ubuntu.(none)>
2014-05-15 15:20:47 -0700

    Minor Changes

--- src/org/bhavmayyin/musicalimageshow/PlaySound.java
+++ src/org/bhavmayyin/musicalimageshow/PlaySound.java
@@ -19,113 +19,117 @@ public class PlaySound {
 	private static ArrayList<String> musicList = new ArrayList<String>();
 	private static int stop = 0;
 	private static int length;
-	
-	
+
 	@SuppressWarnings("static-access")
-	public PlaySound( ArrayList<String> musicFile){
+	public PlaySound(ArrayList<String> musicFile) {
 		this.musicList = musicFile;
-		
 	}
-	
+
 	static void play() {
-		if (stop == 0){
+		if (stop == 0) {
 			FileDescriptor fd = null;
-		   
+
 			try {
-				FileInputStream fis = new FileInputStream(musicList.get(musiccnt));
-		        fd = fis.getFD();
-		        MediaPlayer mp = new MediaPlayer();      
-		        mp.setDataSource(fd);
-		        mp.setAudioStreamType(AudioManager.STREAM_RING);
-		        mp.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+				FileInputStream fis = new FileInputStream(
+						musicList.get(musiccnt));
+				fd = fis.getFD();
+				MediaPlayer mp = new MediaPlayer();
+				mp.setDataSource(fd);
+				mp.setAudioStreamType(AudioManager.STREAM_RING);
+				mp.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
 					@Override
-				public void onCompletion(MediaPlayer mp) {
-					// TODO Auto-generated method stub
-						
-					musiccnt++;
-					if (musiccnt >= musicList.size()){
-						musiccnt = 0;
+					public void onCompletion(MediaPlayer mp) {
+						// TODO Auto-generated method stub
+
+						musiccnt++;
+						if (musiccnt >= musicList.size()) {
+							musiccnt = 0;
+						}
+						mpSet.remove(mp);
+						mp.stop();
+						mp.release();
+						continuePlay();
+
 					}
-					mpSet.remove(mp);
-					mp.stop();
-					mp.release();
-					continuePlay();
-					
-				}
-			});
-		    mpSet.add(mp);
-	        mp.prepare();
-	        
-	      //  Toast.makeText(context," playing -"  + musicList.get(musiccnt),Toast.LENGTH_LONG).show();
-	        mp.start();
-			//mediaPlayer.setLooping(true);
-			
-		} catch(Exception e) {
-			e.printStackTrace();
+				});
+				mpSet.add(mp);
+				mp.prepare();
+
+				// Toast.makeText(context," playing -" +
+				// musicList.get(musiccnt),Toast.LENGTH_LONG).show();
+				mp.start();
+				// mediaPlayer.setLooping(true);
+
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
 		}
-	  }
 	}
-	protected static void continuePlay(){
+
+	protected static void continuePlay() {
 		play();
 	}
-	protected static boolean isplaying(){
+
+	protected static boolean isplaying() {
 		try {
-			
-				if(!mpSet.isEmpty()){
-					return mpSet.iterator().next().isPlaying();
-				} else {
-					return false;
+			if (!mpSet.isEmpty()) {
+				return mpSet.iterator().next().isPlaying();
+			} else {
+				return false;
 			}
-	    	}catch (Exception e) {
-	    		e.printStackTrace();
-	    		return false;
-	    	}
+		} catch (Exception e) {
+			e.printStackTrace();
+			return false;
+		}
 	}
-	protected static void pause(){
+
+	protected static void pause() {
 		try {
-			
-			if(!mpSet.isEmpty()){
+
+			if (!mpSet.isEmpty()) {
 				MediaPlayer mpplayer = mpSet.iterator().next();
 				mpplayer.pause();
 				length = mpplayer.getCurrentPosition();
-			} 
-		
-    	}catch (Exception e) {
-    		e.printStackTrace();
-    		
-    	}
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
-	protected static void resume(){
+
+	protected static void resume() {
 		try {
-			
-			if(!mpSet.isEmpty()){
+
+			if (!mpSet.isEmpty()) {
 				MediaPlayer mpplayer = mpSet.iterator().next();
 				mpplayer.seekTo(length);
 				mpplayer.start();
-			} 
-		
-    	}catch (Exception e) {
-    		e.printStackTrace();
-    		
-    	}
-	}
-    protected static void stop_mediaPlayer() {
-    	try {
-		for (MediaPlayer mp : mpSet) {
-			if(mp!= null){
-				mp.stop();
-				mp.release();
 			}
+		} catch (Exception e) {
+			e.printStackTrace();
+
 		}
-    	}catch (Exception e) {
-    		e.printStackTrace();
-    	}
+	}
+
+	protected static void stop_mediaPlayer() {
+		try {
+			for (MediaPlayer mp : mpSet) {
+				if (mp != null) {
+					mp.stop();
+					mp.release();
+				}
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 		mpSet.clear();
-    }
-	static void stop(){
+	}
+
+	static void stop() {
 		stop_mediaPlayer();
 	}
-	static void stop(int real){
+
+	static void stop(int real) {
 		stop = real;
 		stop_mediaPlayer();
 	}
